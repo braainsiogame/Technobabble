b76ba6b904ca72984163130278ea71688fbb1946
braainsiogame <deadisdead69@gmail.com>
2023-10-27 17:16:20 +0700

    maybe i should make the recipe a different one

--- modularmachinerymaker.bat
+++ modularmachinerymaker.bat
@@ -18,21 +18,5 @@ echo "What are the parts needed? (use a blueprint to json tool)"
 
 read parts 
 
-echo "How many recipes will there be? (int)"
-
-read recipesamount
-
-for ((recipesamount; recipesamount>0; recipesamount--))
-do 
-echo "Input type?"
-read input
-if [ $input != "energy"] && [ $input != "fluid"] && [ $input != "item"]
-then 
-echo "Invalid type!"
-while [ $input != "energy"] && [ $input != "fluid"] && [ $input != "item"] 
-do
-echo "Please reenter the type"
-read input
-
-done 
-done 
+echo "done"
+done
