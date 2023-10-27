e0b1de283d49cb8f5668c69b5e852f55d20b5dbc
braainsiogame <deadisdead69@gmail.com>
2023-10-27 12:27:50 +0700

    we're so barrack

---
+++ modularmachinerymaker.bat
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "What's the registry name? (string)"
+
+read registryname
+
+echo "What's the localized name? (string)"
+
+read localizedname 
+
+echo "Do you need a blueprint? (boolean)"
+
+read blueprintrequired
+if [ $blueprintrequired != "true" ] && [ $blueprintrequired != "false" ] 
+then 
+echo "you will regret not using a boolean"
+fi
+echo "What are the parts needed? (use a blueprint to json tool)"
+
+read parts 
+
+echo "How many recipes will there be? (int)"
+
+read recipesamount
+
+for ((recipesamount; recipesamount>0; recipesamount--))
+do 
+echo "Input type?"
+read input
+if [ $input != "energy"] && [ $input != "fluid"] && [ $input != "item"]
+then 
+echo "Invalid type!"
+while [ $input != "energy"] && [ $input != "fluid"] && [ $input != "item"] 
+do
+echo "Please reenter the type"
+read input
+
+done 
+done 
